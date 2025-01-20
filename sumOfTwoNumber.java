0031f2380ec830e613f1219c5aed70176fdd6c7f
AMANULLAH023HY <amana170829@gmail.com>
2025-01-20 23:31:21 +0530

    basic questions

---
+++ sumOfTwoNumber.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class sumOfTwoNumber {
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        // // sum of two number 
+        // int a  =  sc.nextInt();
+        // int  b =  sc.nextInt(); 
+        // int sum  = a+b;
+
+        // System.out.println("Sum of two Number" +a+ "and"+b+"is :"+sum);
+        // area of circle 
+        // Scanner sc = new Scanner(System.in);
+        // int r  =  sc.nextInt();
+        // // int  b =  sc.nextInt(); 
+        // double areaOFCircle = 3.14*r*r;
+        // System.out.println("Calculate the area of circle is :"+areaOFCircle);
+
+
+        // average of three number 
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // int c = sc.nextInt();
+        // double average = (a+b+c)/3;
+        // System.out.println("Calculate the average of three number is :"+average);
+
+        // area of square 
+        // int side = sc.nextInt();
+        // int areaOfSqure = side*side;
+        // System.out.println("Calculate the area of square is :"+areaOfSqure);
+
+        // What will be the type of result in the following Java code?(Hint : Look at the largest data type among these)
+
+
+        // byte b= 4;
+        // char c ='c';
+        // short s = 512;
+        // int i = 1000;
+        // float f = 3.14f;
+        // double d = 99.9954;
+        // System.out.println("1 :" +(f*b));
+        // System.out.println("2 :" +(i%c));
+        // System.out.println("3 :"+(d*s));
+        // System.out.println("4 :"+((f*b)+(i%c)-(d*s)));
+        // output
+        //1 :12.56
+        // 2 :10
+        // 3 :51197.6448
+        // 4 :-51175.08479862671
+        
+        
+        // Question 5:(Advanced)Will the following statementgive any error in Java?int$=24;Note-Thesequestionsareonlytohelpyoupracticetheconceptsofthischapter.
+
+        int $ = 24;
+
+        System.out.println("use $ :" +$);
+
+
+
+
+
+    }
+}
